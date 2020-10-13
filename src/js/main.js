85920db0f64984d240930e20d5d7117afb77470a
Daniel Lynch <daniellynch.dev@gmail.com>
2020-10-14 00:15:52 +0200

    added slide direction methods

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,11 @@ const carouselContainers = Array.from(document.querySelectorAll(".carousel"));
 
 carouselContainers.forEach(container => {
 	const carousel = new Carousel(container);
+	const next = container.querySelector('.next');
+	const prev = container.querySelector('.prev');
+
+	next.addEventListener('click', carousel.goNextSlide);
+	prev.addEventListener('click', carousel.goPrevSlide);
+
 	carousel.test();
 })
--- src/js/modules/Carousel.js
+++ src/js/modules/Carousel.js
@@ -1,20 +1,34 @@
 export default class Carousel {
 	constructor(container) {
 		this.container = container;
-		this.slides = Array.from(this.container.children);
-		this.activeSlide = this.findActiveClass(this.slides);
+		this.slides = Array.from(this.container.querySelectorAll('img'));
+
+		this.goNextSlide = this.goNextSlide.bind(this);
+		this.goPrevSlide = this.goPrevSlide.bind(this);
 	}
 
-	findActiveClass(arr) {
-		const result = arr.find(element => element.classList.contains('active'));
-		return result;
+	findActiveClass() {
+		const active = this.slides.find(element => element.classList.contains('active'));
+		return active;
 	}
+
+	goNextSlide() {
+		const active = this.slides.indexOf(this.findActiveClass());
+		this.findActiveClass().classList.remove('active');
+		const next = active < this.slides.length - 1 ? active + 1 : 0;
+		this.slides[next].classList.toggle('active');
+	}
+
+	goPrevSlide() {
+		const active = this.slides.indexOf(this.findActiveClass());
+		this.findActiveClass().classList.remove('active');
+		const next = active === 0 ? this.slides.length - 1 : active - 1;
+		this.slides[next].classList.toggle('active');
+	}
+
 
 	// Function for testing purposes.
 	test() {
-		console.log(this.slides);
-		console.log(this.activeSlide);
-		console.log(this.slides.indexOf(this.activeSlide));
-
+		console.log(this);
 	}
 }
