c3765259337a18dbb8ce27cd427fcb0e4b2fa3d8
Daniel Lynch <daniellynch.dev@gmail.com>
2020-10-13 14:46:40 +0200

    minor changes

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,6 @@ const carouselContainers = Array.from(document.querySelectorAll(".carousel"));
 
 carouselContainers.forEach(container => {
 	const carousel = new Carousel(container);
-	carousel.generateSlides(4);
 
 
 	carousel.test("See this? It's working.");
--- src/js/modules/Carousel.js
+++ src/js/modules/Carousel.js
@@ -2,13 +2,12 @@ export default class Carousel {
 	constructor(container) {
 		this.container = container;
 	}
-	generateSlides(amount) {
-		for (let idx = 0; idx < amount; idx++) {
-			const element = document.createElement('div');
-			this.container.appendChild(element);
-		}
 
-	}
+
+
+
+
+
 
 	// Function for testing purposes.
 	test(str) {
