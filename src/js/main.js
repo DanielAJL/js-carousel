1c979ade1a63d97d4c2f6572d5a385914c664b07
Daniel Lynch <daniellynch.dev@gmail.com>
2020-10-13 12:46:05 +0200

    added element creation to Carousel class

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
 import '../scss/main.scss';
 import Carousel from './modules/Carousel';
 
-const carousel = new Carousel();
+const carouselContainers = Array.from(document.querySelectorAll(".carousel"));
+
+carouselContainers.forEach(container => {
+	const carousel = new Carousel(container);
+	carousel.generateSlides(4);
+
+
+	carousel.test("See this? It's working.");
+})
--- src/js/modules/Carousel.js
+++ src/js/modules/Carousel.js
@@ -1,7 +1,18 @@
-// Example Placeholder
-
 export default class Carousel {
-	constructor() {
+	constructor(container) {
+		this.container = container;
+	}
+	generateSlides(amount) {
+		for (let idx = 0; idx < amount; idx++) {
+			const element = document.createElement('div');
+			this.container.appendChild(element);
+		}
+
+	}
+
+	// Function for testing purposes.
+	test(str) {
+		console.log(`${str}`);
 
 	}
 }
