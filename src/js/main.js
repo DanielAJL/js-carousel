0cf5350228f87747c8c00da76eecc62560daf955
Daniel Lynch <daniellynch.dev@gmail.com>
2020-10-15 00:21:53 +0200

    refactoring code

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,19 @@ import Carousel from './modules/Carousel';
 const carouselContainers = Array.from(document.querySelectorAll(".carousel"));
 
 carouselContainers.forEach(container => {
-	const carousel = new Carousel(container);
+	// sliding speed in ms
+	const carousel = new Carousel(container, 1500);
 	const next = container.querySelector('.next');
 	const prev = container.querySelector('.prev');
 
-	next.addEventListener('click', carousel.goNextSlide);
-	prev.addEventListener('click', carousel.goPrevSlide);
+	next.addEventListener('click', () => {
+		carousel.nextSlide('next');
+	});
+
+	prev.addEventListener('click', () => {
+		carousel.nextSlide('prev');
+	});
+
 
 	carousel.test();
 })
--- src/js/modules/Carousel.js
+++ src/js/modules/Carousel.js
@@ -1,10 +1,10 @@
 export default class Carousel {
-	constructor(container) {
+	constructor(container, speed) {
 		this.container = container;
+		this.speed = `${speed}ms`;
 		this.slides = Array.from(this.container.querySelectorAll('img'));
 
-		this.goNextSlide = this.goNextSlide.bind(this);
-		this.goPrevSlide = this.goPrevSlide.bind(this);
+		this.nextSlide = this.nextSlide.bind(this);
 	}
 
 	findActiveClass() {
@@ -12,20 +12,18 @@ export default class Carousel {
 		return active;
 	}
 
-	goNextSlide() {
+	nextSlide(direction) {
+		let next = null;
 		const active = this.slides.indexOf(this.findActiveClass());
 		this.findActiveClass().classList.remove('active');
-		const next = active < this.slides.length - 1 ? active + 1 : 0;
-		this.slides[next].classList.toggle('active');
-		this.slides[next].style.animation = "slide-left 0.5s";
-	}
+		if (direction !== 'prev') {
+			next = active < this.slides.length - 1 ? active + 1 : 0;
+		} else {
+			next = active === 0 ? this.slides.length - 1 : active - 1;
+		}
 
-	goPrevSlide() {
-		const active = this.slides.indexOf(this.findActiveClass());
-		this.findActiveClass().classList.remove('active');
-		const next = active === 0 ? this.slides.length - 1 : active - 1;
 		this.slides[next].classList.toggle('active');
-		this.slides[next].style.animation = "slide-right 0.5s";
+		this.slides[next].style.animation = `slide-${direction} ${this.speed}`;
 	}
 
 
