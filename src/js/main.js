babb8b8ce943f9e2dc7fa081683d0cae640bf0cc
Daniel Lynch <daniellynch.dev@gmail.com>
2020-10-15 15:16:29 +0200

    removed unused code

--- src/js/main.js
+++ src/js/main.js
@@ -4,19 +4,13 @@ import Carousel from './modules/Carousel';
 const carouselContainers = Array.from(document.querySelectorAll(".carousel"));
 
 carouselContainers.forEach(container => {
+
 	// sliding speed in ms
 	const carousel = new Carousel(container, 1500);
+
 	const next = container.querySelector('.next');
 	const prev = container.querySelector('.prev');
 
-	next.addEventListener('click', () => {
-		carousel.nextSlide('next');
-	});
-
-	prev.addEventListener('click', () => {
-		carousel.nextSlide('prev');
-	});
-
-
-	carousel.test();
-})
+	next.addEventListener('click', () => { carousel.nextSlide('next'); });
+	prev.addEventListener('click', () => { carousel.nextSlide('prev'); });
+});
