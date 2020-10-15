df08ea95d5d9a9bc1e682c18fac975079032c084
Daniel Lynch <daniellynch.dev@gmail.com>
2020-10-15 15:17:07 +0200

    added JSDoc params

--- src/js/modules/Carousel.js
+++ src/js/modules/Carousel.js
@@ -1,47 +1,52 @@
 export default class Carousel {
-	constructor(container, speed) {
-		this.container = container;
-		this.speed = `${speed}ms`;
-		this.slides = Array.from(this.container.querySelectorAll('.slide'));
+	/**
+ * @param {HTMLElement} element element containing the 'carousel' class.
+ * @param {number} speed slide speed in ms.
+ */
+	constructor(element, speed) {
+		this.element = element;
+		this.speed = speed;
+		this.slides = Array.from(this.element.querySelectorAll('.slide'));
 
 		this.nextSlide = this.nextSlide.bind(this);
 
 		this.animating = false;
-	}
+	};
 
 	findActiveClass() {
 		const active = this.slides.find(element => element.classList.contains('active'));
 		return active;
-	}
+	};
 
 	nextSlide(direction) {
 		if (this.animating) return;
 
 		let next;
 		const active = this.slides.indexOf(this.findActiveClass());
-		this.findActiveClass().classList.remove('active');
+		const previousSlide = this.findActiveClass();
 
 		if (direction !== 'prev') { next = active < this.slides.length - 1 ? active + 1 : 0; }
 		else { next = active === 0 ? this.slides.length - 1 : active - 1; }
 
 		this.slides[next].classList.toggle('active');
-		this.slides[next].style.animation = `slide-${direction} ${this.speed}`;
+		this.slides[next].classList.toggle('animating');
+
+		this.slides[next].style.animation = `slide-${direction} ${this.speed}ms`;
+		console.log(this.slides[next]);
 		this.animating = true;
 		this.slides[next].addEventListener('animationend', () => {
-			this.afterAnimating(this.slides[next]);
+			this.afterAnimating(this.slides[next], previousSlide);
 		});
 
+	};
 
-	}
+	afterAnimating(activeSlide, previousSlide) {
+		activeSlide.style.animation = '';
 
-	afterAnimating(element) {
-		element.style.animation = '';
+		previousSlide.classList.remove('active');
+		activeSlide.classList.remove('animating');
+
+		activeSlide.removeEventListener('animationend', this.afterAnimating);
 		this.animating = false;
-		element.removeEventListener('animationend', this.afterAnimating);
-	}
-
-	// Function for testing purposes.
-	test() {
-		// console.log(this);
-	}
-}
+	};
+};
